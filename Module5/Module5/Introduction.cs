d7490effa248f2c21c89f6a0763a283dc8b32b92
HelenArkh <alena.arkh@gmail.com>
2020-12-27 21:17:50 +0300

    Создание солюшн и Введение

---
+++ Module5/Module5/Introduction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module5
+{
+    class Introduction
+    {
+
+        public static void Task1()
+        {
+
+            (string name, string[] dishes) User;
+
+            Console.WriteLine("Введите имя пользователя");
+            User.name = Console.ReadLine();
+
+            User.dishes = new string[5];
+            
+            for (int i = 0; i < User.dishes.Length; i++)
+            {
+                Console.WriteLine("Введите любимое блюдо номер {0}",i+1); 
+                User.dishes[i] = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Module5/Module5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Introduction.Task1();
+        }
+    }
+}
