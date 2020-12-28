9497cf69be781fadd0b7dfa08ff1345be97226f6
HelenArkh <alena.arkh@gmail.com>
2020-12-28 15:52:46 +0300

    Задания из раздела Методы

---
+++ Module5/Module5/FavouriteColors.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module5
+{
+    class FavouriteColors
+    {
+        
+       public static void MyFavcolors()
+        {
+            string[] favcolors = new string[3];
+
+            for (int i=0;i<favcolors.Length;i++)
+            {
+                favcolors[i] = ShowColor();
+            }
+
+            foreach (var item in favcolors)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        
+        
+        static string ShowColor()
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            var colour = Console.ReadLine();
+
+            switch (colour)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is yellow");
+                    break;
+            }
+            return colour;
+
+        }
+    }
+}
--- Module5/Module5/Program.cs
+++ Module5/Module5/Program.cs
@@ -6,7 +6,49 @@ namespace Module5
     {
         static void Main(string[] args)
         {
-            Introduction.Task1();
+            //Introduction.Task1();
+            // FavouriteColors.MyFavcolors();
+            GetArrayFromConsole();
+        }
+
+        static string GetDataFromConsole()
+        {
+            return Console.ReadLine();
+        } 
+
+        static string GetDataFromConsole2() => Console.ReadLine();
+
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            int value;
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                for (int j = i + 1; j < result.Length; j++)
+                {
+
+                    if (result[i] > result[j])
+                    {
+                        value = result[i];
+                        result[i] = result[j];
+                        result[j] = value;
+                    }
+                }
+            }
+            foreach (var item in result)
+            {
+                Console.Write(item + " ");
+            }
+
+            return result;
         }
     }
 }
