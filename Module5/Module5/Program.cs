5dc9ae46e2fa088f82c6d31bfb0e2d89ca271fa6
HelenArkh <alena.arkh@gmail.com>
2020-12-29 19:36:37 +0300

    Выполнение заданий по передаче параметров методов

--- Module5/Module5/Program.cs
+++ Module5/Module5/Program.cs
@@ -9,8 +9,52 @@ namespace Module5
             //Introduction.Task1();
             // FavouriteColors.MyFavcolors();
 
-          var array = GetArrayFromConsole(10);
-            ShowArray(array, true);
+           /* int razm = 10;
+            GetArrayFromConsole(ref razm); */
+            // ShowArray(array, true);
+
+            string name = "Евгения";
+            GetName(out name);
+             // ChangeName(ref name);
+              Console.WriteLine(name);
+
+            //  int age = 33;
+              //ChangeAge(age);
+              //Console.WriteLine(age); */
+
+          /*  var arr = new int[] { 1, 2, 3 };
+            var data = 5;
+            BigDataOperation(arr,in data);
+
+            Console.WriteLine(arr[0]); */
+
+        }
+
+        static void BigDataOperation(int[] arr, in int data)
+        {
+          //  data = 4;
+            arr[0] = data;
+        }
+
+        static void GetName(out string name, out string oldname)
+        {
+            oldname = "Евгения";
+            Console.WriteLine("Введите имя");
+            name = Console.ReadLine();
+
+        }
+
+        static void ChangeName(ref string name)
+        {
+            Console.WriteLine("Введите имя");
+            name = Console.ReadLine();
+
+        }
+
+        static void ChangeAge(int age)
+        {
+            Console.WriteLine("Введите возраст");
+            age = Convert.ToInt32(Console.ReadLine());
 
         }
 
@@ -35,7 +79,15 @@ namespace Module5
             }
         }
 
-        static int[] SortArray(int[] array)
+        static void SortArray(in int[] array, out int[] sorteddesc, out int[] sortedasc)
+        {
+            sortedasc = SortArrayAsc(array);
+
+            sorteddesc = SortArrayDesc(array);
+
+        }
+
+        static int[] SortArrayAsc(int[] array)
         {
             int value;
 
@@ -53,11 +105,31 @@ namespace Module5
                 }
             }
             return array;
-
         }
 
-        static int[] GetArrayFromConsole(int num = 5)
+        static int[] SortArrayDesc(int[] array)
         {
+            int value;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+
+                    if (array[i] < array[j])
+                    {
+                        value = array[i];
+                        array[i] = array[j];
+                        array[j] = value;
+                    }
+                }
+            }
+            return array;
+        }
+
+        static int[] GetArrayFromConsole(ref int num)
+        {
+            num = 6;
             var result = new int[num];
 
             for (int i = 0; i < result.Length; i++)
